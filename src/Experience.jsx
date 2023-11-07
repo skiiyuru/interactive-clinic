624e0e57c9dda79979a3215bcb4f780c99281723
StephenKiiyuruNganga <steveknganga@gmail.com>
2023-11-07 14:23:03 +0300

    add sky

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF } from "@react-three/drei"
+import { OrbitControls, Sky, Stage, useGLTF } from "@react-three/drei"
 import { Perf } from "r3f-perf"
 
 export default function Experience() {
@@ -7,14 +7,32 @@ export default function Experience() {
   return (
     <>
       <Perf position="top-left" />
-      <color attach={"background"} args={["#000909"]} />
+      {/* <color attach={"background"} args={["#000909"]} /> */}
 
       <OrbitControls makeDefault />
 
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+      {/* <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} /> */}
 
-      <primitive object={clinic.scene} scale={0.1} position-y={-1} />
+      <Sky
+        distance={450000}
+        sunPosition={[0, 1, 0]}
+        inclination={0}
+        azimuth={0.25}
+      />
+
+      <Stage
+        shadows={{
+          type: "contact",
+          opacity: 0.7,
+          blur: 3,
+        }}
+        preset={"soft"}
+        environment={"city"}
+        intensity={0.07}
+      >
+        <primitive object={clinic.scene} scale={0.1} position-y={-1} />
+      </Stage>
     </>
   )
 }
